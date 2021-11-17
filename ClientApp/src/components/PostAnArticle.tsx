10624379ae0d8a9b1470213c7b1fa2d03c21169d
seamussmith <student.141504@worcesterschools.net>
2021-11-17 11:36:06 -0500

    Data is synced and ready to submit

--- ClientApp/src/components/PostAnArticle.tsx
+++ ClientApp/src/components/PostAnArticle.tsx
@@ -1,17 +1,36 @@
+import React, { useState } from "react"
 
 export function PostAnArticle({})
 {
+    const [disabled, setDisabled] = useState(false);
+    const [author, setAuthor] = useState("");
+    const [title, setTitle] = useState("");
+    const [tags, setTags] = useState("");
+    const [content, setContent] = useState("");
+    async function submitArticle(e: React.FormEvent<HTMLFormElement>)
+    {
+        console.log(author, title, tags, content);
+        e.preventDefault();
+    }
     return (
         <div>
-            <form action="/api/article/" method="POST">
-                <label htmlFor="Author">Author</label>
-                <input name="Author" type="text" required />
-                <label htmlFor="Title">Title</label>
-                <input name="Title" type="text" required />
-                <label htmlFor="Tags">Tags</label>
-                <input name="Tags" type="text" />
-                <label htmlFor="Content">Content</label>
-                <input name="Content" type="text" required />
+            <form action="/api/article/" onSubmit={submitArticle}>
+                <div>
+                    <label htmlFor="Author">Author</label>
+                    <input onChange={x => setAuthor(x.currentTarget.value)}name="Author" type="text" required disabled={disabled} />
+                </div>
+                <div>
+                    <label htmlFor="Title">Title</label>
+                    <input onChange={x => setTitle(x.currentTarget.value)} name="Title" type="text" required disabled={disabled}/>
+                </div>
+                <div>
+                    <label htmlFor="Tags">Tags</label>
+                    <input onChange={x => setTags(x.currentTarget.value)} name="Tags" type="text" disabled={disabled}/>
+                </div>
+                <div>
+                    <label htmlFor="Content">Content</label>
+                    <input onChange={x => setContent(x.currentTarget.value)}name="Content" type="text" required disabled={disabled} />
+                </div>
                 <input type="submit" />
             </form>
         </div>
