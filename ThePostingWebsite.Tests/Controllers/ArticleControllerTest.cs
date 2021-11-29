59bae566dc838e85d770a9007494603551343714
seamussmith <student.141504@worcesterschools.net>
2021-11-29 12:44:37 -0500

    Add more tests

--- ThePostingWebsite.Tests/Controllers/ArticleControllerTest.cs
+++ ThePostingWebsite.Tests/Controllers/ArticleControllerTest.cs
@@ -6,24 +6,25 @@ using ThePostingWebsite.Controllers;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
 using ThePostingWebsite.Models;
+using Xunit.Abstractions;
 
 namespace ThePostingWebsite.Tests.Controllers;
 
 public class ArticleControllerTest
 {
-    public ArticleControllerTest()
+    private readonly ITestOutputHelper output;
+    public ArticleControllerTest(ITestOutputHelper _output)
     {
+        output = _output;
     }
-    [Fact]
-    public void ApiArticle_ReturnsAnIndexOfArticles()
+    private DbContextOptions<ArticleContext> makeMockDB(int count = 10)
     {
-        var articleCount = 10;
         var options = new DbContextOptionsBuilder<ArticleContext>()
-            .UseInMemoryDatabase(databaseName: "MovieListDatabase")
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
             .Options;
         using (var init = new ArticleContext(options))
         {
-            Enumerable.Range(1, articleCount).ToList().ForEach(
+            Enumerable.Range(1, count).ToList().ForEach(
                 x => init.Add(new Article()
                 {
                     Author = "mock",
@@ -33,16 +34,65 @@ public class ArticleControllerTest
                 })
             );
             init.SaveChanges();
+            var i = 0;
+            foreach (var x in init.Articles.Include(x => x.Comments))
+            {
+                ++i;
+                Enumerable.Range(1, count).ToList().ForEach(
+                    y => x.Comments.Add(new Comment()
+                    {
+                        Author= "mock",
+                        Content = "mock"
+                    })
+                );
+            }
+            output.WriteLine(count.ToString());
+            init.SaveChanges();
         }
+        return options;
+    }
+    private ArticleController makeArticleController(ArticleContext opt)
+    {
+        return new ArticleController(opt, Mock.Of<ILogger<ArticleController>>());
+    }
+    [Fact]
+    public void ArticleController_GetArticles_ReturnsAnIndexOfArticles()
+    {
+        var articleCount = 10;
+        var options = makeMockDB(articleCount);
 
         using (var context = new ArticleContext(options))
         {
-            var logger = Mock.Of<ILogger<ArticleController>>();
-            var articleController = new ArticleController(context, logger);
+            var articleController = makeArticleController(context);
             var res = articleController.GetArticles();
             // Value should not be null
             Assert.NotNull(res.Value);
             Assert.True(res.Value!.Count == articleCount);
+        }
+    }
+    [Fact]
+    public void ArticleController_GetArticle_ShouldReturnTheArticleWithId()
+    {
+        var options = makeMockDB(1);
+
+        using (var context = new ArticleContext(options))
+        {
+            var articleController = makeArticleController(context);
+            var res = articleController.GetArticle(1);
+            Assert.NotNull(res.Value);
+        }
+    }
+    [Fact]
+    public void ArticleController_GetArticleComments_ShouldReturnComments()
+    {
+        var articleCount = 10;
+        var options = makeMockDB(articleCount);
+        using (var context = new ArticleContext(options))
+        {
+            var articleController = makeArticleController(context);
+            var res = articleController.GetArticleComments(1);
+            Assert.NotNull(res.Value);
+            Assert.Equal(articleCount, res.Value!.Count);
         }
     }
 }
