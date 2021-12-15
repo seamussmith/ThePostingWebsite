d1b740049dd5fa6e0889fde8b95294043a4236b2
seamussmith <student.141504@worcesterschools.net>
2021-12-15 12:19:59 -0500

    Basic article editing

--- ThePostingWebsite/ClientApp/src/components/EditArticle.tsx
+++ ThePostingWebsite/ClientApp/src/components/EditArticle.tsx
@@ -1,12 +1,37 @@
+import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
+import { Button, FormGroup, Input, Label } from "reactstrap";
+import { Article } from "../models/Article";
 import { AjaxForm } from "./AjaxForm";
 
 export function EditArticle({}) {
-    let params = useParams<{ id: string }>();
+    const params = useParams<{ id: string }>();
+    const [article, setArticle] = useState<Article | null>(null);
+    const [contentFieldText, setContentFieldText] = useState("");
+    useEffect(() => {
+        fetch(`api/article/${params.id}`)
+            .then<Article>((x) => x.json())
+            .then((x) => {
+                setArticle(article);
+                setContentFieldText(x.content);
+            });
+    }, []);
     return (
         <div>
             <AjaxForm method="PUT" action={`api/article/${params.id}`}>
-                <input type="textarea" />
+                <FormGroup>
+                    <Label htmlFor="Content">Content</Label>
+                    <Input
+                        name="Content"
+                        type="textarea"
+                        required
+                        value={contentFieldText}
+                        onChange={(e) => setContentFieldText(e.target.value)}
+                    />
+                </FormGroup>
+                <FormGroup className="mt-1">
+                    <Button type="submit">Edit</Button>
+                </FormGroup>
             </AjaxForm>
         </div>
     );
